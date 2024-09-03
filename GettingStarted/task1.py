ab86f68dc29649bace2e85174e77adf6af066bae
MsHayesCCC <144285264+MsHayesCCC@users.noreply.github.com>
2024-09-03 09:48:18 +0100

    Update task1.py

--- GettingStarted/task1.py
+++ GettingStarted/task1.py
@@ -1,6 +1,6 @@
 # Author: Ms Hayes
 # Date: Sept 2024
-# Description: Textbook Chapter 1 Tasks
+# Description: Textbook Chapter 1 Tasks Pg 12
 # Task 1: Use two print() functions, write and test the code to display the string "Goodbye!" twice on the same line.
 
 print("Goodbye!", end=" ")
