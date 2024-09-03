36d360f429fd2cf24673024b9338e3cc08530708
MsHayesCCC <144285264+MsHayesCCC@users.noreply.github.com>
2024-09-03 09:46:14 +0100

    Create task3.py
    Textbook pg 12 Task 3

---
+++ GettingStarted/task3.py
@@ -0,0 +1,62 @@
+# Author: Ms Hayes
+# Date: Sept 2024
+# Description: Textbook Chapter 3 Tasks
+# Task 3: Using ordinary characters and special characters, write the code to create a pattern
+# in the shape of:
+# - a face
+# - the letter X
+# - a triangle
+# - a small circle
+# - a large circle
+
+# Face
+print("\t   FACE")
+print("\t *******")
+print("\t*\t*")
+print("\t* *   * *")
+print("\t*   o   *")
+print("\t*\t*")
+print("\t*  ***  *")
+print("\t*\t*")
+print("\t *******")
+
+print("\n")
+# The letter X
+print("\t Letter X")
+print("\t*\t*")
+print("\t *     *")
+print("\t  *   *")
+print("\t   * *")
+print("\t    *")
+print("\t   * *")
+print("\t  *   *")
+print("\t *     *")
+print("\t*\t*")
+
+print("\n")
+# A triangle
+print("     Triangle")
+print("\t*")
+print("       ***")
+print("      *****")
+print("     *******")
+print("    *********")
+
+print("\n")
+# A small circle
+print("     Small Circle")
+print("\t  ___ ")
+print("\t /   \\")
+print("\t \___/")
+
+print("\n")
+# A large circle
+print("     Large Circle")
+print("\t  _____ ")
+print("\t /     \\")
+print("\t/       \\")
+print("\t|       |")
+print("\t|       |")
+print("\t\\       /")
+print("\t \\_____/")
+
