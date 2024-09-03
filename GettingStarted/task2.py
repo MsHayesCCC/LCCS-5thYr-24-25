5a80c04f0249e720b93bc0f24d6c30afe4e61e4b
MsHayesCCC <144285264+MsHayesCCC@users.noreply.github.com>
2024-09-03 09:48:04 +0100

    Update task2.py

--- GettingStarted/task2.py
+++ GettingStarted/task2.py
@@ -1,6 +1,6 @@
 # Author: Ms Hayes
 # Date: Sept 2024
-# Description: Textbook Chapter 2 Tasks
+# Description: Textbook Chapter 1 Tasks Pg 12
 # Task 2: Using an argument, change the following code to display a full stop after the string: print("No way").
 # Amend this code to add a tab between the words inside the brackets.
 
